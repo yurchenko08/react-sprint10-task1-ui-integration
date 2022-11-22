557dc65b76a1f367d4baf330167db30089983c9b
yurchenko08 <yurchenko08@gmail.com>
2022-11-23 00:17:24 +0200

    finished part#1

--- src/tasks/01-ui-integration/PostsList.jsx
+++ src/tasks/01-ui-integration/PostsList.jsx
@@ -1,42 +1,51 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import store from "./store";
 
 export default class PostsList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            // TODO Get the posts and users lists from the store instead
-            posts : [],
-            authors : [],
-        };
+    this.state = {
+      // TODO Get the posts and users lists from the store instead
+      posts: store.getState().posts,
+      authors: store.getState().authors,
+    };
 
+    // TODO subscribe to the store
+  }
+  componentDidMount() {
+    store.subscribe(this.onStoreUpdated);
+  }
 
-        // TODO subscribe to the store
-    }
+  onStoreUpdated = () => {
+    this.setState({
+      posts: store.getState().posts,
+      authors: store.getState().authors,
+    });
+  };
 
-    onStoreUpdated = () => {
-        // TODO Update the list of posts and users in state to re-render
-    }
+  render() {
+    const { posts, authors } = this.state;
 
-    render() {
-        const {posts, authors} = this.state;
+    const renderedPosts = posts.map((post) => {
+      const author = authors.find(
+        (author) => author.authorId === post.authorId
+      ) || { name: "Unknown" };
+      const { name } = author;
 
-        const renderedPosts = posts.map(post => {
-            const author = authors.find(author => author.authorId === post.authorId) || {name : "Unknown"};
-            const {name} = author;
+      return (
+        <li key={post.id}>
+          {post.title}, by {name}
+        </li>
+      );
+    });
 
-            return <li key={post.id }>{post.title}, by {name}</li>;
-        });
-
-        return (
-            <div>
-                <h4>Posts</h4>
-                <ul>
-                    {renderedPosts}
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h4>Posts</h4>
+        <ul>{renderedPosts}</ul>
+      </div>
+    );
+  }
 }
